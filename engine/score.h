79cea9d2331899e9b565400621e81f5fd267a2d9
Minelt-1 <121693426+Minelt-1@users.noreply.github.com>
2023-05-21 22:36:54 +0200

    Create score.h

---
+++ engine/score.h
@@ -0,0 +1,10 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include "../board.h"
+
+int getScore(Board &board, std::int16_t row, std::int16_t col);
+
+#endif 
+
+//MAKE A FUNCTION THAT RETURNS TWO PAIRS POINTS (SCORE, TIMES) AND BESTMOVE (INDEX INDEX) 
